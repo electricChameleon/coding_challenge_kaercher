3c60c7b4bf20a7386d3b81f7676467ec03e8f18d
electricChameleon <143426521+electricChameleon@users.noreply.github.com>
2023-08-28 17:29:48 +0200

    Add files via upload

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function submitFile() {
+    console.log("submit this!");
+}
+</script>
+
+<template>
+    <input type="file">
+    <button @click="submitFile">Submit</button>
+</template>
