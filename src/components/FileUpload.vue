b7069aff0f47ef8e171a89fb8773607fdf82f6cc
electricCameleon <blackrose-21@web.de>
2023-09-03 10:53:57 +0200

    [Upload] JSON file can now be uploaded; path will now parsed into an array

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,14 +1,44 @@
 <script setup>
 import { ref } from 'vue'
+const pathArray = ref([]);
 
-const count = ref(0)
+function handleFileUpload(event) {
+    const file = event.target.files[0];
+    if (!file) return;
 
-function submitFile() {
-    console.log("submit this!");
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        try {
+            const jsonData = JSON.parse(e.target.result);
+
+            if (jsonData.path && Array.isArray(jsonData.path)) {
+                pathArray.value = [];
+                Array.prototype.push.apply(pathArray.value, jsonData.path);
+            } else {
+                alert('Invalid JSON format: "path" property is missing or not an array.');
+            }
+        } catch (error) {
+            alert('Error parsing JSON file: ' + error.message);
+        }
+    };
+
+    reader.readAsText(file);
 }
 </script>
 
 <template>
-    <input type="file">
-    <button @click="submitFile">Submit</button>
+    <div>
+        <input type="file" @change="handleFileUpload" accept=".json" />
+        <div v-if="pathArray.length > 0">
+            <h2>Path Array:</h2>
+            <ul>
+                <li v-for="(point, index) in pathArray" :key="index">
+                    {{ point }}
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
+  
+  
